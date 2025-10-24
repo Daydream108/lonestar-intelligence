16970e65002d449987e624962a9a5d675b3130f4
Daydream108 <brettpascall@yahoo.com>
2025-10-23 21:13:24 -0500

    feat: Update project dependencies and build config
    Refactors project configuration by updating dependencies in `package.json` to newer versions and adjusting `tsconfig.json` for improved build settings. Includes minor adjustments to `vite.config.ts` to align with updated build targets and module resolution.

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: { 
-    sourcemap: false, 
-    minify: "esbuild", 
-    target: "es2018", 
-    chunkSizeWarningLimit: 1200, 
-    rollupOptions: { 
-      output: { 
-        manualChunks: { 
-          vendor: ["react","react-dom"] 
+  build: {
+    sourcemap: false,
+    minify: "esbuild",
+    target: "es2018",
+    chunkSizeWarningLimit: 1200,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ["react", "react-dom"]
         }
       }
     }
   }
-})
+});
